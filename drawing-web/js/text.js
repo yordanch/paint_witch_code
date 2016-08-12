cdae4728c4be664397235a55c939a2b43b2b0ff6
yordanch <yordanchvargas@hotmail.com>
2016-08-12 12:14:57 -0500

    Create text.js
    v2.2 - create class text

---
+++ drawing-web/js/text.js
@@ -0,0 +1,10 @@
+(function(textD) {
+	function DrawText() {
+		this.startLienzo();
+	}
+	DrawText.prototype.DrawText = function(e){
+		
+	}
+	textD.DrawText = DrawText;
+})(window);
+var startC = new DrawText();
